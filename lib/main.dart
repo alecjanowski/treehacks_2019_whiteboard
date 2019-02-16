ad3fd4b878c7176851b246f54038bf680b5887f3
lzrdkng260 <acid@stanford.edu>
2019-02-16 13:05:55 -0800

    Changed MyApp to something simpler

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,29 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
+  @override //Makes it obvious that we are overwriting a method withing StatelessWidget
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Wyteboard'),
+            backgroundColor: Color(0xfeb12e),
+          ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
+
   }
+  
 }
 
+/*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -109,3 +111,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
