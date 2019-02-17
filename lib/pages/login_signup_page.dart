7016eaa86161951f9739604d2545bfae8c485abe
bennybermuda <laux6459@stanford.edu>
2019-02-16 23:07:22 -0800

    add logo to login/signup

--- lib/pages/login_signup_page.dart
+++ lib/pages/login_signup_page.dart
@@ -105,7 +105,7 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
         .platform == TargetPlatform.iOS;
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text('Flutter login demo'),
+          title: new Text(''),
         ),
         body: Stack(
           children: <Widget>[
@@ -187,11 +187,11 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
     return new Hero(
       tag: 'hero',
       child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
         child: CircleAvatar(
-          backgroundColor: Colors.transparent,
-          radius: 48.0,
-          child: Image.asset('assets/Untitled-Artwork.png'),
+          backgroundColor: Colors.orange,
+          radius: 118.0,
+          child: Image.asset('assets/logoWhite.png'),
         ),
       ),
     );
@@ -258,7 +258,7 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
             elevation: 5.0,
             shape: new RoundedRectangleBorder(
                 borderRadius: new BorderRadius.circular(30.0)),
-            color: Colors.blue,
+            color: Colors.orange,
             child: _formMode == FormMode.LOGIN
                 ? new Text('Login',
                 style: new TextStyle(fontSize: 20.0, color: Colors.white))
